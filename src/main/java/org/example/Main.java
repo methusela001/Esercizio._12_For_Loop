de49628642d02f671db62bb5d14522e080fde66a
Methusela Fernando <136374272+methusela001@users.noreply.github.com>
2023-07-30 12:39:37 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//Use a for-loop to print all the number from 5 to 25 (including the upper and lower bound)
+
+        int x=5;
+        for (int i = 0; i <=25 ; i++) {
+            System.out.println((i));
+        }
     }
 }
